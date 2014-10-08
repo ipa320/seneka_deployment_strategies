9b03faade140823855756e5ebb62cc6e2f4e1d75
ipa-mig-mc <muhammad.chughtai@ipa.frauhofer.de>
2014-10-08 16:09:17 +0200

    created 'sensor_placement_hypervisor' node

---
+++ seneka_sensor_placement/src/sensor_placement_hypervisor.cpp
@@ -0,0 +1,94 @@
+/****************************************************************
+ *
+ * Copyright (c) 2014
+ *
+ * Fraunhofer Institute for Manufacturing Engineering
+ * and Automation (IPA)
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Project name: SeNeKa
+ * ROS stack name: seneka_deployment_strategies
+ * ROS package name: seneka_sensor_placement
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Author: Muhammad Bilal Chughtai, email:Muhammad.Chughtai@ipa.fraunhofer.de
+ *
+ * Date of creation: October 2014
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the Fraunhofer Institute for Manufacturing
+ *     Engineering and Automation (IPA) nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License LGPL as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License LGPL for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License LGPL along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************/
+
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+#include <sstream>
+
+
+int main(int argc, char **argv)
+{
+
+  ros::init(argc, argv, "sensor_placement_hypervisor");
+
+  ros::NodeHandle n;
+
+  ros::Publisher quanjo_maneuver_pub = n.advertise<std_msgs::String>("quanjo_maneuver_topic", 1000);
+
+  ros::Rate loop_rate(10);
+
+  int count = 0;
+  while (ros::ok())
+  {
+    /**
+     * This is a message object. You stuff it with data, and then publish it.
+     */
+    std_msgs::String msg;
+
+    std::stringstream ss;
+    ss << "this is quanjo_maneuver_msg message " << count;
+    msg.data = ss.str();
+
+ //   ROS_INFO("%s", msg.data.c_str());
+
+
+    quanjo_maneuver_pub.publish(msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    ++count;
+  }
+
+
+  return 0;
+}
